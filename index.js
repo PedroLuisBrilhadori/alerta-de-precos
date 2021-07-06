25fbd1f36821bab4de64f294f106c4dd41b374e8
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-07-06 16:06:17 -0300

    notificando

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 const notifier = require('node-notifier');
+var readline = require('readline-sync');
 
-let url = 'https://www.google.com/search?q=mglu3';
+let acao = readline.question("insira qual ação você quer monitorar: ") || 'mglu3';
+let minimo = readline.question("insira qual o preço minimo para notificação: ") || 21.00;
+let url = `https://www.google.com/search?q=${acao}`;
 
 (async () =>{
     const browser = await puppeteer.launch();
@@ -10,8 +13,12 @@ let url = 'https://www.google.com/search?q=mglu3';
     const preco = await page.evaluate(() =>{
         return document.getElementsByClassName("IsqQVc NprOob")[0].innerText;
     });
-    console.log(preco);
-    notifier.notify(preco);
-    await page.screenshot({path: 'teste.png'});
+    console.log(preco);    
+    notificacao(preco) ? notifier.notify(preco) : null;
     await browser.close();
-})();
+})(); 
+
+function notificacao(preco){
+    n = preco.replace(',', '.')
+    return n < minimo
+}
