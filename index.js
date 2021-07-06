d5790f906b051cc4ab91285cf24a34f0155b4fdf
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-07-06 19:59:35 -0300

    for await sem notificacao

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const puppeteer = require('puppeteer');
 const notifier = require('node-notifier');
-var readline = require('readline-sync');
+let readline = require('readline-sync');
 
 let acao = readline.question("insira qual ação você quer monitorar: ") || 'mglu3';
 let minimo = readline.question("insira qual o preço minimo para notificação: ") || 21.00;
 let url = `https://www.google.com/search?q=${acao}`;
 
-(async () =>{
+async function pegarPreco(){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     const preco = await page.evaluate(() =>{
         return document.getElementsByClassName("IsqQVc NprOob")[0].innerText;
     });
-    console.log(preco);    
-    notificacao(preco) ? notifier.notify(preco) : null;
+    await page.screenshot({path: 'teste.png'});
     await browser.close();
-})(); 
+    return preco;
+}
+
+
+async function mostrarPreco(){
+    const preco = await pegarPreco();
+    if(notificacao(preco) == true){
+        new Promise(resolve => notifier.notify(preco)); 
+        minimo =  readline.question("insira qual o preço minimo para notificação: ") || 21.00;
+    }
+    console.log(preco);    
+}
 
 function notificacao(preco){
-    n = preco.replace(',', '.')
+    let n = preco.replace(',', '.')
     return n < minimo
+}
+
+async function* asyncGenerator() {
+    var i = 0;
+    while (i < 15) {
+      yield i++;
+    }
+  }
+  
+(async function() {
+  for await (let num of asyncGenerator()) {
+      await mostrarPreco();
+      await sleep(1700)
+  }
+})();
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
